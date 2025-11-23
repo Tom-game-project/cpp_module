b8d0ddf453dea4a3a05d2a033d3828fc4b5419c5
muranaka tomonari <82128211+Tom-game-project@users.noreply.github.com>
2025-11-23 16:15:45 +0900

    init commit

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+#include <string>
+#include <iostream>
+
+Zombie::Zombie(std::string name): name(name) {}
+
+Zombie::~Zombie() {
+	std::cout << this->name << " is dead" << std::endl;
+}
+
+void Zombie::announce() {
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
