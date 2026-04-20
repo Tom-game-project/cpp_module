a249792b54a6c93f9ae21007c417ed1d5938d5f2
muranaka tomonari <tom.ipynb@gmail.com>
2026-04-20 15:45:52 +0900

    add ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifndef __BUREAUCRAT_HPP__
+# define __BUREAUCRAT_HPP__
+
+#include <exception>
+#include <string>
+
+#define GradeTooHighExceptionMsg "Error: Bureaucrat grade is too high (must be >= 1)."
+
+#define GradeTooLowExceptionMsg "Error: Bureaucrat grade is too low (must be <= 150)."
+
+class Bureaucrat {
+private:
+  int grade;
+  const std::string name;
+public:
+  class GradeTooHighException: public std::exception {
+    public:
+      virtual const char* what() const throw() {
+          return "Error: Bureaucrat grade is too high (must be >= 1).";
+      }
+  };
+
+  class GradeTooLowException: public std::exception {
+    public:
+      virtual const char* what() const throw() {
+          return "Error: Bureaucrat grade is too low (must be <= 150).";
+      }
+  };
+
+  Bureaucrat();
+  Bureaucrat(const std::string& name, const int grade);
+  Bureaucrat(const Bureaucrat& other);
+  Bureaucrat &operator=(const Bureaucrat& other);
+  ~Bureaucrat();
+
+  std::string getName() const;
+  int getGrade() const;
+
+  // throw error
+  void increment(); 
+  // throw error
+  void decrement();
+};
+
+// クラス定義の "外" に記述します
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif
