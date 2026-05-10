13d8d3854b38a11e356d07c38c506e87f92b41be
muranaka tomonari <tom.ipynb@gmail.com>
2026-05-10 17:42:46 +0900

    コメントの追加

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,6 +2,11 @@
 
 #include "BitcoinExchange.hpp"
 
+// data.csvの用意
+// ```
+// wget https://cdn.intra.42.fr/document/document/46100/cpp_09.tgz && tar -xzvf *.tgz && mv cpp_09/data.csv . && rm cpp_09.tgz && rmdir cpp_09
+// ```
+
 int main(int argc, char *argv[]) {
   if (argc == 2) {
     return func(argv[1]);
